d0d7ae4ce23f4f3a2a36e1ab7af47922ae8c6327
EythorE <eye23@hi.is>
2019-03-22 23:03:53 +0000

    tetris

--- v1.js
+++ v1.js
@@ -181,20 +181,8 @@ window.onload = function init()
          }
      }
 
-
-
-    for ( var i = 0; i < heigth; i++ ) {
-        arr[i] = [];
-        for ( var j = 0; j < depth; j++ ){
-            arr[i][j] = [];
-            for ( var k = 0; k < width; k++ ){
-                arr[i][j][k] = 0;
-            }
-        }
-    }
      gameLoop();
 }
-var arr = [], width = 5, depth = 5, heigth = 5;
 var checked = false;
 const moveSpeed = 1.0;
 var moveFB = 0;
@@ -205,30 +193,101 @@ var spinX1 = 0;
 var spinY1 = 0;
 
 
+var arr = [], width = 6, depth = 6, height = 7;
+for ( var x = 0; x < width; x++ ) {
+    arr[x] = [];
+    for ( var y = 0; y < height+3; y++ ){
+        arr[x][y] = [];
+        for ( var z = 0; z < depth; z++ ){
+            arr[x][y][z] = 0;
+        }
+    }
+}
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function Piece()
+{
+
+    this.init = function()
+    {
+        //// initialize piece
+        this.type = getRandomInt(1,3);
+
+        // random top position
+        this.yPos = height+1;
+        this.xPos = getRandomInt( 1, width-1);
+        this.zPos = getRandomInt( 1, depth-1);
+
+
+        // Always make same piece the just rotate for randomness
+        this.indicies = []; //+1 or -1 for each axis
+        if(this.type == 1){
+            this.indicies[0] = [0, 0, 0]; // should always be [0,0,0]
+            this.indicies[1] = [1, 0, 0];
+            this.indicies[2] = [-1, 0, 0];
+        }
+        if(this.type == 2){
+            this.indicies[0] = [0, 0, 0]; // should always be [0,0,0]
+            this.indicies[1] = [1, 0, 0];
+            this.indicies[2] = [0, 1, 0];
+        }
+
+
+        // Random rotation
+        var xRotate = getRandomInt(0,5);
+        var yRotate = getRandomInt(0,5);
+        var zRotate = getRandomInt(0,5);
+        for(var i = 0; i<xRotate; i++){ this.rotateX(); }
+        for(var i = 0; i<yRotate; i++){ this.rotateY(); }
+        for(var i = 0; i<zRotate; i++){ this.rotateZ(); }
+
+        // Put this new piece in arr
+        for(var i = 0; i<3; i++){ arr[ this.xPos+this.indicies[i][0] ][ this.yPos+this.indicies[i][1] ][ this.zPos+this.indicies[i][2] ] = this.type }
+
+        ////
+    }
+
+    this.drop = function()
+    {
+        for(var i = 0; i<3; i++){ arr[ this.xPos+this.indicies[i][0] ][ this.yPos+this.indicies[i][1] ][ this.zPos+this.indicies[i][2] ] = 0 }
+        this.yPos = this.yPos-1;
+        for(var i = 0; i<3; i++){ arr[ this.xPos+this.indicies[i][0] ][ this.yPos+this.indicies[i][1] ][ this.zPos+this.indicies[i][2] ] = this.type }
+        // if piece hit rock bottom /placeholder
+        if(this.yPos==0){
+            this.init();
+        }
+    }
+
+    this.rotateX = function()
+    {
+
+    }
+    this.rotateY = function()
+    {
+
+    }
+    this.rotateZ = function()
+    {
+
+    }
+
+    this.init()
+}
+
+var p = new Piece();
+var time = 0;
 
 function gameLoop()
 {
-    arr[0][0][0] = 1;
-    arr[0][1][0] = 1;
-    arr[0][2][0] = 1;
-    arr[1][0][0] = 1;
-
-    arr[0][0][1] = 2;
-    arr[1][0][1] = 2;
-    arr[2][0][1] = 2;
-    arr[3][0][1] = 2;
-
-    arr[0+2][0][0+2] = 1;
-    arr[0+2][1][0+2] = 1;
-    arr[0+2][2][0+2] = 1;
-    arr[1+2][0][0+2] = 1;
-
-    arr[1][1][0] = 1;
-    arr[1][1][1] = 1;
-    arr[1][1][2] = 1;
-    arr[1][0][2] = 1;
-
+    if(time%40==0){
+        p.drop();
+    }
+    time++;
     render();
     requestAnimFrame(gameLoop);
 }
@@ -283,17 +342,18 @@ function render()
         spinX1 = spinX;
         spinY1 = spinY;
     }
-    var field_rotation = mult(rotateY(spinY1), rotateX(spinX1));
-    var field_translation = translate(-(width-1)/2, -(heigth-1)/2, -(depth-1)/2)
+    //var field_rotation = mult(rotateY(spinY1), rotateX(spinX1));
+    var field_rotation = rotateY(spinY1);
+    var field_translation = translate(-(width-1)/2, -(height-1)/2, -(depth-1)/2)
     var field_mt = mult(field_rotation, field_translation)
 
     var s = scalem(15/32, 15/32, 15/32);
     var t;
 
-    for ( var i = 0; i < heigth; i++ ) {
-        for ( var j = 0; j < depth; j++ ){
-            for ( var k = 0; k < width; k++ ){
-                var type = arr[i][j][k];
+    for ( var x = 0; x < width; x++ ) {
+        for ( var y = 0; y < height+3; y++ ){
+            for ( var z = 0; z < depth; z++ ){
+                var type = arr[x][y][z];
                 if(type == 1){
                     gl.uniform4fv( gl.getUniformLocation(program, "ambientProduct" ), flatten(redAmbientProduct) );
                     gl.uniform4fv( gl.getUniformLocation(program, "diffuseProduct" ), flatten(redDiffuseProduct) );
@@ -305,7 +365,7 @@ function render()
                     gl.uniform4fv( gl.getUniformLocation(program, "specularProduct"), flatten(blueSpecularProduct) );
                 } else continue;
 
-                var t = translate(i, j, k);
+                var t = translate(x, y, z);
                 var mt = mult(t,s);
                 mt = mult(field_mt, mt)
                 gl.uniformMatrix4fv(mtLoc, false, flatten(mt) );
